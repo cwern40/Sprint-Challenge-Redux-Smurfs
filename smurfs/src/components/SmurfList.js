bcc0e543db33b235ba88a9fca84a4e3c0f1075b2
cwern40 <chris.wernli@hotmail.com>
2019-07-20 10:30:34 -0600

    I created the SmuftList.js file and imported it into the App.js file I also created initial state in the reducer

---
+++ smurfs/src/components/SmurfList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class SmurfList extends React.Component {
+
+    render() {
+        return (
+            <div className="smurf-list">
+                <h1>Smurf List</h1>
+            </div>
+        )
+    }
+}
+
+export default SmurfList;
