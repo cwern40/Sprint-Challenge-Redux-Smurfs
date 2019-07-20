677124e299633580a1ef6e0c066698658506dbab
cwern40 <chris.wernli@hotmail.com>
2019-07-20 11:33:01 -0600

    I created the onchagne handler for the addsmurfs form

--- smurfs/src/components/AddSmurf.js
+++ smurfs/src/components/AddSmurf.js
@@ -10,12 +10,17 @@ class AddSmurf extends React.Component {
         }
     }
 
+    onChange(event) {
+        event.preventDefault()
+        this.setState({ [event.target.name]: event.target.value})
+    }
+
     render() {
         return(
             <form>
-                <input type="text" name="name" placeholder="Name" value={this.state.name} />
-                <input type="text" name="age" placeholder="Age" value={this.state.age} />
-                <input type="text" name="height" placeholder="Height" value={this.state.height} />
+                <input type="text" name="name" placeholder="Name" value={this.state.name} onChange={this.onChange} />
+                <input type="text" name="age" placeholder="Age" value={this.state.age} onChange={this.onChange} />
+                <input type="text" name="height" placeholder="Height" value={this.state.height} onChange={this.onChange} />
                 <button type="submit">Add Smurf</button>
             </form>
         )
